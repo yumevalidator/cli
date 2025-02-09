ed1c4ac9792a5a9a8793fa81aa17b571c65ca08d
Victor Mak <contact@htetaung.com>
2025-02-09 09:25:26 +0800

    feat: re_enable functional testing

--- src/testing_agent.py
+++ src/testing_agent.py
@@ -3,5 +3,5 @@ from .functional_testing_agent import start_functional_testing_agent
 
 def start_testing():
     start_visual_testing_agent()
-    #start_functional_testing_agent()
+    start_functional_testing_agent()
 
